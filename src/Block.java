270e7941f1022dd66f50bcbb12be444747fe6106
Suporte04 <Suporte04@Suporte04-PC>
2019-04-01 17:18:22 -0300

    Parte 1 concluida

--- src/Block.java
+++ src/Block.java
@@ -9,6 +9,7 @@ public class Block {
     public String previousHash;
     private String data; //our data will be a simple message.
     private long timeStamp; //as number of milliseconds since 1/1/1970.
+    private int nonce;
 
 	//Block Constructor.
 	public Block(String data,String previousHash ) {
@@ -18,12 +19,24 @@ public class Block {
                 this.hash = calculateHash();
 	}
         
+       
         public String calculateHash() {
-            String calculatedhash = StringUtil.applySha256( previousHash +
-			Long.toString(timeStamp) +
-			data );
+		String calculatedhash = StringUtil.applySha256( 
+				previousHash +
+				Long.toString(timeStamp) +
+				Integer.toString(nonce) + 
+				data 
+				);
+		return calculatedhash;
+	}
         
-            return calculatedhash;
-        }
+        public void mineBlock(int difficulty) {
+		String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0" 
+		while(!hash.substring( 0, difficulty).equals(target)) {
+			nonce ++;
+			hash = calculateHash();
+		}
+		System.out.println("Block Mined!!! : " + hash);
+	}
 }
 
